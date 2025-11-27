a4886cf9c706231dce1b70e91eab820b012aa26d
mist-eun-lee <dzainie@gmail.com>
2025-11-26 19:48:33 -0800

    Capstone Project

---
+++ Capstone/Integrating Text-to-Speech in Language Instruction/story_content/5W7PFVcm25b_captions.js
@@ -0,0 +1,1 @@
+window.globalLoadJsAsset('story_content/5W7PFVcm25b_captions.js', {"captions":[{"langCode":"en-US","data":"WEBVTT%0D%0A%0D%0ANOTE%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.105.35604.0%0D%0A%0D%0A00:00:00.061%20--%3E%2000:00:02.206%0D%0A%20First,%20select%20a%20staff%20voice.%0D%0A%0D%0A"}]})
